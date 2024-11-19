86c1b693e806e954b46ac1fb115ddc3c1fd0e4e6
MUHAMMAD SHAHID KHAN <afridi.sk1720@gmail.com>
2024-11-19 19:06:49 +0500

    FirstCommit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react"
+import Navbar from "@/scenes/navbar"
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<string>("home")
+  return (
+    <div className="app bg-gray-20">
+      <Navbar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+    </div>
+  )
+}
+
+export default App
